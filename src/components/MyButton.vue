6728a30ff52d5a33d3f02a1108d9e760604c9e24
jasonwang <iou8611614@gmail.com>
2021-03-09 20:57:59 +0800

    start likeIG project with Vue3

---
+++ src/components/MyButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="item in state.items" :key="item">{{item}}</li>
+        </ul>  
+    </div>
+</template>
+
+<script>
+import {reactive} from "vue";
+export default {
+    setup(){
+        const state = reactive({
+            items:['item1', 'item2', 'item3']
+        });
+        return{
+            state,
+        }
+    },
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <MyButton/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import MyButton from "@/components/MyButton.vue"
+
+export default {
+  name: "Button",
+  components: {
+    MyButton
+  }
+};
+</script>
